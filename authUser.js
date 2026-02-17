ddb6d653ea90e220691d4b45ff504ccc478ed284
amanlal <alal09635@gmail.com>
2026-02-16 22:37:57 -0800

    connecting server to database created jwt authentication for authUser.js downloaded additional packages for connection between express and database

--- authUser.js
+++ authUser.js
@@ -1,15 +1,184 @@
+const jwt = require('jsonwebtoken')
+const jwt_secret = 'super_secret_key'
+
 const express = require('express')
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
 
-let users = []
+// delete
+const requireAuth = (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Missing Authorization header' })
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const payload = jwt.verify(token, jwt_secret)
+        req.userId = payload.userId
+        next()
+    } catch (err) {
+        res.status(401).json({ error: 'Invalid or expired token' })
+    }
+}
+
+
+/* const requireAuth = (req, res, next) => {
+    if (!currentUser) {
+        return res.status(401).json({error: 'Authentication required'})
+    }
+    next()
+} */
+
+// validate note middleware
+// used in POST
+const validateNote = (req, res, next) => {
+    const { title, content } = req.body
+
+    if(!title) {
+        return res.status(400).json({error: 'title is required'})
+    }
+    if(!content) {
+        return res.status(400).json({error: 'content is required'})
+    }
+
+    next()
+}
+
+// validate id
+// utilised in GET and DELETE by id
+const validateId = (req, res, next) => {
+    const id = Number(req.params.id)
+
+    if(isNaN(id)) {
+        return res.status(400).json({error: 'Invalid ID'})
+    }
+
+    req.id = id
+    next()
+}
+
+const requireJsonBody = (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({error: 'Body is empty'})
+    }
+
+    next()
+}
+
+const users = []
 let currentUser = null
 let userId = 1
+const notes = []
+let nextId = 1
 
 app.get('/', (req, res) => {
     res.send('server is running')
 })
 
+// get all users
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+// Register new users
+app.post('/register', (req, res) => {
+    const { username, password } = req.body
+
+    if (!username || !password) {
+        return res.status(400).json({error: 'Username and password are required'})
+    }
+
+    // check if user exists
+    const userExist = users.find(u => u.username === username)
+    if (userExist) {
+        return res.status(409).json({error: 'User already exists'})
+    }
+
+    const newUser = {
+        id : userId++,
+        username,
+        password
+    }
+    users.push(newUser)
+
+    res.status(201).json({message: 'User registered successfully'})
+})
+
+// Login
+app.post('/login', (req, res) => {
+    const { username, password } = req.body
+
+    if (!username || !password) {
+        return res.status(400).json({error: 'Username and password are required'})
+    }
+
+    // verify user
+    const user = users.find(u => u.username === username && u.password === password)
+    if (user) {
+        currentUser = user
+    }
+    else {
+        return res.status(400).json({error: 'Incorrect password or username'})
+    }
+   // delete this code after testing
+
+    const token = jwt.sign(
+        { userId: user.id },
+        jwt_secret,
+        {expiresIn: '1h'}
+    )
+
+    res.json({ token })
+    //delete
+})
+
+// Logout
+app.post('/logout', (req, res) => {
+    currentUser = null
+
+    res.json({message: 'Logged out successfully'})
+})
+
+// GET all notes with status 200
+app.get('/notes', requireAuth, (req, res) => {
+    res.json(notes)
+})
+
+// Add valid notes to array and return note with status 201
+app.post('/notes', requireAuth, requireJsonBody, validateNote, (req, res) => {
+    const { title, content } = req.body
+
+    const newNote = {
+        id: nextId++,
+        title,
+        content
+    }
+    notes.push(newNote)
+
+    res.status(201).json(newNote)
+
+})
+
+// DELETE note by id
+// return status 204 upon success
+// return status 404 if failure
+app.delete('/notes/:id', requireAuth, validateId, (req, res) => {
+    const noteIndex = notes.findIndex(u => u.id === req.id)
+
+    if(noteIndex === -1) {
+        return res.status(404).json({error: 'Note not found'})
+    }
+    notes.splice(noteIndex, 1)
+
+    res.status(204).send()
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`)
+})
 
