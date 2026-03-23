a7df79db6bc8256fc8aac9cbbf014d592163e8bb
amanlal <alal09635@gmail.com>
2026-03-22 18:02:27 -0700

    implemented validateNote

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -2,16 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 const requireAuth = require('../middleware/authMiddleware')
+const { validateNote } = require('../middleware/validationMiddleware')
 const { getNotes, createNote, updateNote, deleteNote } = require('../controllers/noteController')
 
 // Get note
 router.get('/', requireAuth, getNotes)
 
 // Add note
-router.post('/', requireAuth, createNote)
+router.post('/', requireAuth, validateNote, createNote)
 
 // Update note
-router.put('/:id', requireAuth, updateNote)
+router.put('/:id', requireAuth, validateNote, updateNote)
 
 // Delete note
 router.delete('/:id', requireAuth, deleteNote)
